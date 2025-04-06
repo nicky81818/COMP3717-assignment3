c3c25e38569f9f6cd2a459277d3e280e52430500
NICOLE_HSU <nicole.d.hsu@gmail.com>
2025-04-05 21:50:31 -0700

    Add styling to home page

--- app/src/main/java/com/example/assignment/Home.kt
+++ app/src/main/java/com/example/assignment/Home.kt
@@ -3,26 +3,22 @@ package com.example.assignment
 import android.annotation.SuppressLint
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -45,20 +41,45 @@ fun Home(quoteState: QuoteState) {
                     IconButton(onClick = {searchVisible = false}) {Icon(Icons.Default.Close, contentDescription = "Close") }
                 }
             } else {
-                IconButton(onClick = {searchVisible = true}) {
-                    Icon(Icons.Default.Search, contentDescription = "Search")
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    IconButton(onClick = { searchVisible = true }) {
+                        Icon(Icons.Default.Search, contentDescription = "Search")
+                    }
+                    Text("Search for an Entry")
                 }
             }
 
             LazyColumn {
                 item {
-                    Text(quoteState.quote?.text ?: "none")
-                    Text(quoteState.quote?.author?.name ?: "Unknown")
+                    Column(horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.background(Color(0xFFFCF5E5))){
+                        Text(quoteState.quote?.text ?: "none",
+                            fontFamily = FontFamily.Cursive,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 50.sp,
+                            textAlign = TextAlign.Center)
+                        val author = quoteState.quote?.author?.name ?: "Unknown"
+                        Text("-$author",
+                            fontSize = 35.sp,
+                            textAlign = TextAlign.Center)
+                    }
                 }
                 items(entries.size) {
                     val date = entries[it].date
-                    DateTextField(date)
-                    Text("${entries[it].summary}")
+                    Card(
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 10.dp
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    ){
+                        DateTextField(date)
+                        Text("${entries[it].summary}",
+                            fontSize = 25.sp,
+                            modifier = Modifier.padding(20.dp))
+
+                    }
                 }
             }
         }
@@ -74,5 +95,8 @@ fun formatDate(date: Date): String {
 @Composable
 fun DateTextField(date: Long){
     val dateDate = Date(date)
-    Text(formatDate(dateDate))
+    Text(formatDate(dateDate),
+        fontWeight = FontWeight.Bold,
+        fontSize = 35.sp,
+        modifier = Modifier.padding(10.dp))
 }
